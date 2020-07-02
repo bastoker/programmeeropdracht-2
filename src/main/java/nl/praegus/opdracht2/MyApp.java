735acb5dcc51d28dda29bd690f6098aa18ed9ede
Bas Stoker <bas.stoker@praegus.nl>
2020-07-02 10:17:32 +0200

    Voorbeelduitwerking toegevoegd

--- src/main/java/nl/praegus/opdracht2/MyApp.java
+++ src/main/java/nl/praegus/opdracht2/MyApp.java
@@ -5,7 +5,11 @@ import nl.praegus.opdracht2.database.DatabaseImpl;
 import nl.praegus.opdracht2.pojo.Auteur;
 import nl.praegus.opdracht2.pojo.Blog;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.format;
 
 /***
  *    ██████╗ ██████╗  █████╗ ███████╗ ██████╗ ██╗   ██╗███████╗     ██████╗ ██████╗ ██████╗ ██████╗  █████╗  ██████╗██╗  ██╗████████╗    ██████╗
@@ -56,7 +60,30 @@ public class MyApp {
         List<Blog> allBlogs = database.getAllBlogs();
 
         // TODO: Bereken het totaal aantal woorden en toon deze aan de gebruiker:
+        Map<Auteur, Integer> woordenPerAuteur = new HashMap<>();
 
+        // Initialiseer de map met 0 voor elke auteur.
+        // Dit voorkomt o.a. dat we auteurs die nog geen blogs geschreven hebben
+        // niet genoemd worden in het overzicht:
+        for (Auteur auteur: allAuteurs) {
+            woordenPerAuteur.put(auteur, 0);
+        }
+
+        // Voor het aantal woorden kiezen we ervoor om spaties en newlines
+        // als scheidingsteken te gebruiken.
+        //
+        // We gebruiken hier de default compute-functie van de Map-interface
+        // voor een iets functionelere stijl om dit op te lossen:
+        for (Blog b : allBlogs) {
+            woordenPerAuteur.compute(
+                    b.getAuteur(),
+                    (a, c) -> c + b.getBlogpost().split("[ \n]").length);
+        }
+        allAuteurs.forEach(
+                a -> System.out.println(format("Auteur %s heeft %s woorden geschreven", a.getNaam(), woordenPerAuteur.get(a)))
+        );
+
+        System.out.println("\n\n");
 
 
         System.out.println("\n\n");
